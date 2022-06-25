a58f3b11f98cc77847e4a7061d2407e790b23e54
mitoma <mutetheradio@gmail.com>
2022-06-26 08:10:32 +0900

    シンボリックリンクのサポート
    - 指し先を依存パスとして追加する
    - Linkオブジェクト自体もハッシュ計算に入れる

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod sver_config;
 use std::{
     collections::{BTreeMap, HashMap},
     error::Error,
-    path::{Path, PathBuf},
+    path::{Component, Path, PathBuf},
 };
 
 use self::filemode::FileMode;
@@ -104,7 +104,7 @@ fn calc_hash_string(
     for (path, oid_and_mode) in source {
         hasher.update(path);
         match oid_and_mode.mode {
-            FileMode::Blob => {
+            FileMode::Blob | FileMode::Link => {
                 // Q. Why little endian?
                 // A. no reason.
                 hasher.update(u32::from(oid_and_mode.mode).to_le_bytes());
@@ -209,17 +209,51 @@ fn collect_path_and_excludes(
     p.push(path);
     p.push("sver.toml");
 
+    let mut current_path_and_excludes: HashMap<String, Vec<String>> = HashMap::new();
+
     if let Some(entry) = repo.index()?.get_path(p.as_path(), 0) {
         debug!("sver.toml exists. path:{:?}", String::from_utf8(entry.path));
         let default_config =
             SverConfig::load_profile(repo.find_blob(entry.id)?.content(), "default")?;
+        current_path_and_excludes.insert(path.to_string(), default_config.excludes.clone());
         path_and_excludes.insert(path.to_string(), default_config.excludes);
-
         for dependency_path in default_config.dependencies {
             collect_path_and_excludes(repo, &dependency_path, path_and_excludes)?;
         }
     } else {
+        current_path_and_excludes.insert(path.to_string(), vec![]);
         path_and_excludes.insert(path.to_string(), vec![]);
+    }
+
+    // include synbolic link
+    for entry in repo.index()?.iter() {
+        let containable = containable(entry.path.as_slice(), &current_path_and_excludes);
+        if containable && FileMode::Link == FileMode::from(entry.mode) {
+            let path = String::from_utf8(entry.path)?;
+            let mut buf = PathBuf::new();
+            buf.push(path);
+            buf.pop();
+
+            let blob = repo.find_blob(entry.id)?;
+            let link_path = String::from_utf8(blob.content().to_vec())?;
+            let link_path = Path::new(&link_path);
+            for link_components in link_path.components() {
+                debug!("link_component:{:?}", link_components);
+                match link_components {
+                    Component::ParentDir => {
+                        buf.pop();
+                    }
+                    Component::Normal(path) => buf.push(path),
+                    Component::RootDir => {}
+                    Component::CurDir => {}
+                    Component::Prefix(_prefix) => {}
+                }
+            }
+
+            let link_path = buf.to_str().ok_or("path is invalid")?;
+            debug!("collect link path. path:{}", link_path);
+            collect_path_and_excludes(repo, link_path, path_and_excludes)?;
+        }
     }
     Ok(())
 }
