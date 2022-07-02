d24eef846dee62fcfeb29e9723c36edff4d36b2f
mitoma <mutetheradio@gmail.com>
2022-07-03 07:22:28 +0900

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,8 @@ pub fn verify_sver_config() -> Result<Vec<String>, Box<dyn Error>> {
                     let path = if target_path == "" { "" } else { target_path };
                     if let Some(result) = config.verify(path, &repo).unwrap() {
                         let mut result_str = String::new();
-                        result_str.push_str(&format!("[NG]\t{}/sver.toml:{}\n", target_path, profile));
+                        result_str
+                            .push_str(&format!("[NG]\t{}/sver.toml:{}\n", target_path, profile));
                         result_str.push_str(&format!(
                             "\t\tinvalid_dependency:{:?}\n",
                             result.invalid_dependencies
