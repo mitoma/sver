31a980e8bb9646dbd26ffab1ad0e4e2e6b7644ed
mitoma <mutetheradio@gmail.com>
2022-07-09 14:57:26 +0900

    リリース手順をスクリプト化

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eux
+TAG="$1"
+REPO_ROOT=$(git rev-parse --show-toplevel)
+VERSION=$(sver calc .)
+LINUX_TMP=$(mktemp -d)
+WINDOWS_TMP=$(mktemp -d)
+
+cd "$REPO_ROOT"
+gh run download -n "sver-linux-${VERSION}" --dir "$LINUX_TMP"
+cd "$LINUX_TMP"
+chmod +x sver
+zip "sver_${TAG}_linux_amd64.zip" *
+echo "$LINUX_TMP/sver_${TAG}_linux_amd64.zip"
+
+cd "$REPO_ROOT"
+gh run download -n "sver-windows-${VERSION}" --dir "$WINDOWS_TMP"
+cd "$WINDOWS_TMP"
+zip "sver_${TAG}_windows_amd64.zip" *
+echo "$WINDOWS_TMP/sver_${TAG}_windows_amd64.zip"
+
+cd "$REPO_ROOT"
+gh release create $TAG --generate-notes
+gh release upload $TAG \
+   "$LINUX_TMP/sver_${TAG}_linux_amd64.zip" \
+   "$WINDOWS_TMP/sver_${TAG}_windows_amd64.zip"
