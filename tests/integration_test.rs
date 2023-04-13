54d323e12d95d977b79b3b53c042aa3efa162942
mitoma <mutetheradio@gmail.com>
2023-04-14 00:04:50 +0900

    add test case

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1015,6 +1015,58 @@ fn invalid_no_default_repository() {
 
 // repo layout
 // .
+// + service1/sver.toml → no default
+// + service2/sver.toml → dependency = [ "service1:default" ]
+#[test]
+fn invalid_ref_to_no_config_repository() {
+    initialize();
+
+    // setup
+    let repo = setup_test_repository();
+    add_blob(
+        &repo,
+        "service1/README.md",
+        "hello"
+            .as_bytes(),
+    );
+    add_blob(
+        &repo,
+        "service2/sver.toml",
+        "
+        [default]
+        dependencies = [
+            \"service1:default\",
+        ]"
+        .as_bytes(),
+    );
+    commit(&repo, "setup");
+
+    let sver_repo = SverRepository::new(&calc_target_path(&repo, "service2")).unwrap();
+
+    // exercise
+    let ValidationResults {
+        has_invalid,
+        mut results,
+    } = sver_repo.validate_sver_config().unwrap();
+
+    // verify
+    assert_eq!(has_invalid, false);
+    assert_eq!(results.len(), 1);
+
+    if let Some(ValidationResult::Valid {
+        calcuration_target: CalculationTarget { profile, path },
+        ..
+    }) = results.pop()
+    {
+        assert_eq!(path, "service2");
+        assert_eq!(profile, "default");
+    } else {
+        assert!(false, "this line will not be execute");
+    }
+}
+
+// repo layout
+// .
 // + service1/hello.txt
 #[test]
 fn init_on_basedirectory() {
